ee4670db9c3b26743db1000171c2827e301bf907
Ezinne <ekalu.fk@gmail.com>
2023-05-12 09:17:22 +0100

    save addf

--- add_func.py
+++ add_func.py
@@ -1,17 +1,19 @@
-input_string = input("Please enter your calculation")
+input_string = input("Please enter your calculation:")
 
 class Calculator:
+    def __init__(self):
+        self.words = {"plus": "+"}
+    
     def add_numbers(self, input_string):
         numbers = []
         for item in input_string.split():
-            try:
-                numbers.append(float(item))
-            except ValueError:
-                pass
+            if item in self.words:
+                numbers.append(self.words[item])
+            else:
+                try:
+                    numbers.append(float(item))
+                except ValueError:
+                    pass
         return sum(numbers)
-calculator = Calculator()
-result = calculator.add_numbers(input_string)
-print(result)  
-
 #result = calculator.add_numbers("1 + 1 + 12")
 #print(result)  
